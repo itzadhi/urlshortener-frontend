cc97b200fbe28fd30ca78213d992b7de94847533
adhit7 <adhithyanalan@gmail.com>
2024-03-26 20:39:03 +0530

    final changes

--- src/screens/ActivationMail.jsx
+++ src/screens/ActivationMail.jsx
@@ -1,21 +1,23 @@
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { setUserStatus } from '../slices/authSlice';
 import { toast } from 'react-toastify';
 
 const ActivationMail = () => {
+  const navigate = useNavigate();
   let { id } = useParams();
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(setUserStatus(id));
     toast.info(
-      'Your account has been activated, you will be redirected to the home page',
+      'Your account has been activating, soon you will be redirected to the home page',
       {
         position: 'top-right',
       }
     );
+    navigate('/login');
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
