9b5616cdaaf777860cab22e8e06d7c503ecb4ebd
adhit7 <adhithyanalan@gmail.com>
2024-03-26 17:41:55 +0530

    minor change

--- src/slices/apiSlice.js
+++ src/slices/apiSlice.js
@@ -1,7 +1,7 @@
 import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: 'http://localhost:4000',
+  baseUrl: 'https://urlshortener-backend-7xpj.onrender.com',
   credentials: 'include',
 });
 
